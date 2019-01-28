36fd5dc09a8a84742bba56807d4b8c4ebc5abb24
manlorna <lora99w@gmail.com>
2019-01-28 18:43:35 +0100

    FEAT. Solución incidencia #4. Se sube los test realizados para comprobar el correcto funcionamiento del user con campos extras

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -4,7 +4,8 @@ from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
-
+from .forms import FormSignUp,UserForm,ProfileForm
+from django.contrib.auth import authenticate
 from base import mods
 
 
@@ -79,3 +80,46 @@ class AuthTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(Token.objects.filter(user__username='voter1').count(), 0)
+
+    def test_newUser(self):
+        data = {'username': 'username1', 'first_name': 'first_name1', 'last_name': 'last_name1','email': 'email1@gmail.com','sex': 'Hombre','location': 'location1','birth_date': '2018-01-28','password1': 'testnew1', 'password2': 'testnew1'}
+        response = self.client.post('/authentication/createUser/', data, format='json')
+        #Comprobamos que llegamos a la  vista
+        self.assertEqual(response.status_code, 200)
+
+        #Cargamos el formulario de la vista con los datos(data)
+        form=FormSignUp(data)
+        self.assertTrue(form)
+        self.assertTrue(form.is_valid())
+        form.save() #Guardamos al nuevo usuario
+
+        #Comprobamos que el usuario existe realmente
+        self.assertEqual(User.objects.filter(username='username1').count(), 1)
+
+    def test_newUserRepeated(self):
+        #Creamos el primer usuario
+        data = {'username': 'username1', 'first_name': 'first_name1', 'last_name': 'last_name1','email': 'email1@gmail.com','sex': 'Hombre','location': 'location1','birth_date': '2018-01-28','password1': 'testnew1', 'password2': 'testnew1'}
+        response = self.client.post('/authentication/createUser/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        form=FormSignUp(data)
+        self.assertTrue(form.is_valid())
+        form.save() #Guardamos al nuevo usuario 1
+        self.assertEqual(User.objects.filter(username='username1').count(), 1) #Comprobamos que se guarda el primer usuario
+
+        #Creamos el segundo usuario(con el mismo username)
+        data2 = {'username': 'username1', 'first_name': 'first_name1', 'last_name': 'last_name2','email': 'email2@gmail.com','sex': 'Hombre','location': 'location1','birth_date': '2018-01-28','password1': 'testnew2', 'password2': 'testnew2'}
+        response = self.client.post('/authentication/createUser/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        form=FormSignUp(data2)
+        self.assertEqual(form.is_valid(),False)
+
+         
+    def test_newUserPassDifferent(self):
+        #Creamos el primer usuario
+        data = {'username': 'username1', 'first_name': 'first_name1', 'last_name': 'last_name1','email': 'email1@gmail.com','sex': 'Hombre','location': 'location1','birth_date': '2018-01-28','password1': 'testneaaw1', 'password2': 'testnew1'}
+        response = self.client.post('/authentication/createUser/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        form=FormSignUp(data)
+        self.assertEqual(form.is_valid(),False)
+
+
